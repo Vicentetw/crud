3fcfbec80700dcc331c98700a3ae8e29d45868a4
Vicentetw <perrottavicente@gmail.com>
2022-02-24 13:01:54 -0300

    initial commit creando server y tsconfig

---
+++ server/build/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let suma = 0;
+let x = 20;
+let y = 30;
+function sumar(x, Y) {
+    return x + Y;
+}
+;
+console.log(sumar(20, 30));
+console.log('sumar(20,30)');
---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+let suma =0;
+let x=20;
+let y=30;
+
+function sumar(x:number, Y:number){
+    return x + Y;
+};
+console.log(sumar(20,30));
+console.log('sumar(20,30)');
